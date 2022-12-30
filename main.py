0139b762de104b54a2949a710a015396a41007da
Daniel Dreke <daniel.dreke@mailbox.org>
2022-12-30 12:17:58 +0100

    add route post/user to add user/userpubkey or update post counter

--- main.py
+++ main.py
@@ -2,6 +2,18 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+users = dict()
+
 @app.get("/")
 def read_root():
     return {"message": "Server is up and running!"}
+
+@app.post("/user")
+async def add_or_update_user(userpubkey: str):
+    if userpubkey not in users:
+        users[userpubkey] = 0
+    else:
+        users[userpubkey] += 1
+    counter = users[userpubkey]
+    print(counter, userpubkey)
+    return users
